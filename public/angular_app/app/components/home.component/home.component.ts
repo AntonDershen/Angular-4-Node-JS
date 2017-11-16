4582fb42f2d8c0078d35462f4188e7ec0fb68752
Dershen Anton <a.dershen@croc.ru>
2017-11-16 16:49:02 +0300

    Добавлена логика основного приложения

--- public/angular_app/app/components/home.component/home.component.ts
+++ public/angular_app/app/components/home.component/home.component.ts
@@ -57,43 +57,114 @@ export class HomeComponent {
     keyboardInput(event: KeyboardEvent) {
         switch(event.code) { 
             case "ArrowUp": { 
-               //statements; 
-               this.up();
-               this.initRandomValue();
-               break;
+                this.upShift();
+                this.upSum();
+                this.initRandomValue();
+                break;
             } 
             case "ArrowRight": { 
-               this.right();
-               this.initRandomValue();
-               break; 
+                this.rightShift();
+                this.rightSum();
+                this.initRandomValue();
+                break; 
             } 
             case "ArrowLeft": { 
-                // this.left();
-                // this.initRandomValue();
+                this.leftShift();
+                this.leftSum();
+                this.initRandomValue();
                 break; 
             } 
             case "ArrowDown": { 
-               //statements; 
-               break; 
+                this.downShift();
+                this.downSum();
+                this.initRandomValue();
+                break; 
             } 
-         } 
+        } 
     }
 
-    right() {
-        for (var _i = 0; _i < 4; _i+=4) {
-            var preventValue = 0;
+    rightSum() {
+        for (var _i = 3; _i < 16; _i+=4) {
+            var preventValue = -1;
+            var preventIndex = 0;
             for(var _g = 0; _g < 4; _g++){
-                if(this.list[_i + _g]!=0){
-                    if(preventValue == 0){
-                        preventValue = this.list[_i + _g];
+                if(preventValue == 0){
+                    if(this.list[_i - _g] != 0){
+                        preventValue = this.list[_i - _g]
+                        preventIndex = _i - _g
                         continue;
                     }
-                    if(preventValue == this.list[_i + _g]){
-                        this.list[_i + _g] = preventValue * 2;
+                }
+                if(preventValue != 0){
+                    if(this.list[_i - _g] == preventValue){
+                        this.list[preventIndex] = 2 * preventValue;
+                        this.list[_i - _g] = 0;
+                        preventIndex = 0;
+                        preventValue = -1;
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i - _g] != preventValue){
+                        preventIndex = _i - _g;
+                        preventValue = this.list[_i - _g];
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    rightShift() {
+        for (var _i = 3; _i < 16; _i+=4) {
+            var preventValue = -1;
+            var preventIndex = 0;
+            for(var _g = 0; _g < 4; _g++){
+                if(preventValue == 0){
+                    if(this.list[_i - _g] != 0){
+                        this.list[preventIndex] = this.list[_i - _g]
+                        this.list[_i - _g] = 0;
+                        preventIndex--;
+                        _g = _i - preventIndex;
                         preventValue = 0;
                         continue;
                     }
-                    if(preventValue != this.list[_i + _g]){
+                }
+                if(preventValue != 0){
+                    if(this.list[_i - _g] == 0){
+                        preventValue = 0;
+                        preventIndex = _i - _g;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    leftSum() {
+        for (var _i = 0; _i < 16; _i+=4) {
+            var preventValue = -1;
+            var preventIndex = 0;
+            for(var _g = 0; _g < 4; _g++){
+                if(preventValue == 0){
+                    if(this.list[_i + _g] != 0){
+                        preventValue = this.list[_i + _g]
+                        preventIndex = _i + _g
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] == preventValue){
+                        this.list[preventIndex] = 2 * preventValue;
+                        this.list[_i + _g] = 0;
+                        preventIndex = 0;
+                        preventValue = -1;
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] != preventValue){
+                        preventIndex = _i + _g;
                         preventValue = this.list[_i + _g];
                         continue;
                     }
@@ -102,29 +173,146 @@ export class HomeComponent {
         }
     }
 
-    up() {
+    leftShift() {
+        for (var _i = 0; _i < 16; _i+=4) {
+            var preventValue = -1;
+            var preventIndex = 0;
+            for(var _g = 0; _g < 4; _g++){
+                if(preventValue == 0){
+                    if(this.list[_i + _g] != 0){
+                        this.list[preventIndex] = this.list[_i + _g]
+                        this.list[_i + _g] = 0;
+                        preventIndex++;
+                        _g = preventIndex - _i;
+                        preventValue = 0;
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] == 0){
+                        preventValue = 0;
+                        preventIndex = _i + _g;
+                        continue;
+                    }
+                }
+                
+            }
+        }
+    }
+
+    upSum() {
         for (var _i = 0; _i < 4; _i++) {
             var preventValue = -1;
             var preventIndex = 0;
             for(var _g = 0; _g < 16; _g+=4){
-                    if(preventValue == 0){
-                        if(this.list[_i + _g] != 0){
-                            this.list[preventIndex] = this.list[_i + _g]
-                            this.list[_i + _g] = 0;
-                            preventIndex +=4;
-                            _g = preventIndex - _i;
-                            preventValue = 0;
-                            continue;
-                        }
-                    }
-                    if(preventValue != 0){
-                        if(this.list[_i + _g] == 0){
-                            preventValue = 0;
-                            preventIndex = _i + _g;
-                            continue;
-                        }
+                if(preventValue == 0){
+                    if(this.list[_i + _g] != 0){
+                        preventValue = this.list[_i + _g]
+                        preventIndex = _i + _g
+                        continue;
                     }
                 }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] == preventValue){
+                        this.list[preventIndex] = 2 * preventValue;
+                        this.list[_i + _g] = 0;
+                        preventIndex = 0;
+                        preventValue = -1;
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] != preventValue){
+                        preventIndex = _i + _g;
+                        preventValue = this.list[_i + _g];
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    upShift() {
+        for (var _i = 0; _i < 4; _i++) {
+            var preventValue = -1;
+            var preventIndex = 0;
+            for(var _g = 0; _g < 16; _g+=4){
+                if(preventValue == 0){
+                    if(this.list[_i + _g] != 0){
+                        this.list[preventIndex] = this.list[_i + _g]
+                        this.list[_i + _g] = 0;
+                        preventIndex +=4;
+                        _g = preventIndex - _i;
+                        preventValue = 0;
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] == 0){
+                        preventValue = 0;
+                        preventIndex = _i + _g;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    downSum() {
+        for (var _i = 0; _i < 4; _i++) {
+            var preventValue = -1;
+            var preventIndex = 0;
+            for(var _g = 12; _g >= 0; _g-=4){
+                if(preventValue == 0){
+                    if(this.list[_i + _g] != 0){
+                        preventValue = this.list[_i + _g]
+                        preventIndex = _i + _g
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] == preventValue){
+                        this.list[preventIndex] = 2 * preventValue;
+                        this.list[_i + _g] = 0;
+                        preventIndex = 0;
+                        preventValue = -1;
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] != preventValue){
+                        preventIndex = _i + _g;
+                        preventValue = this.list[_i + _g];
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    downShift() {
+        for (var _i = 0; _i < 4; _i++) {
+            var preventValue = -1;
+            var preventIndex = 0;
+            for(var _g = 12; _g >= 0; _g-=4){
+                if(preventValue == 0){
+                    if(this.list[_i + _g] != 0){
+                        this.list[preventIndex] = this.list[_i + _g]
+                        this.list[_i + _g] = 0;
+                        preventIndex -=4;
+                        _g = preventIndex - _i;
+                        preventValue = 0;
+                        continue;
+                    }
+                }
+                if(preventValue != 0){
+                    if(this.list[_i + _g] == 0){
+                        preventValue = 0;
+                        preventIndex = _i + _g;
+                        continue;
+                    }
+                }
+            }
         }
     }
 
